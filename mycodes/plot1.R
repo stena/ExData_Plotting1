fcfe7a7d58c77f3af4148a964c435405b8d6d636
stena <sten.malmlund@gmail.com>
2014-05-10 18:28:24 +0300

    Replaced a wrong file

---
+++ mycodes/plot1.R
@@ -0,0 +1,10 @@
+library(sqldf)
+
+
+sqls <- paste0("select * from file where Date = '1/2/2007' or Date = '2/2/2007'")
+hp_cons <- read.csv.sql("household_power_consumption.txt", sql = sqls, sep=';', header=TRUE)
+
+hist(hp_cons$Global_active_power,col='red',main='Global Active Power', xlab='Global Active Power (kilowatts)')
+
+dev.copy(png, file = 'plot1.png')
+dev.off()
