0a7b3e782653eb1ba27b05b9b64cdad797a934f0
stena <sten.malmlund@gmail.com>
2014-05-10 18:45:59 +0300

    Changed text sizes in labels and legends

--- mycodes/plot1.R
+++ mycodes/plot1.R
@@ -4,7 +4,8 @@ library(sqldf)
 sqls <- paste0("select * from file where Date = '1/2/2007' or Date = '2/2/2007'")
 hp_cons <- read.csv.sql("household_power_consumption.txt", sql = sqls, sep=';', header=TRUE)
 
-hist(hp_cons$Global_active_power,col='red',main='Global Active Power', xlab='Global Active Power (kilowatts)')
+hist(hp_cons$Global_active_power,col='red',main='Global Active Power', 
+     xlab='Global Active Power (kilowatts)', cex.lab=0.9)
 
 dev.copy(png, file = 'plot1.png')
 dev.off()
